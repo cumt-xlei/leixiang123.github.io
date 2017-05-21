c9c38533492d79f272e72388f9ec76693f800ec2
lx <1098519043@qq.com>
2017-05-21 19:36:50 +0800

    修改

--- OOAD/duck/src/com/lee/test/DuckTest.java
+++ OOAD/duck/src/com/lee/test/DuckTest.java
@@ -8,9 +8,15 @@ public class DuckTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//创建Duck并选择具体的子类对象
 		Duck mallard = new MallardDuck();
+		
+		//给对象的QuackBehavior属性动态设置实现类
 		mallard.setQuackBehavior(new Squeak());
+		
+		//调用performQuack方法
 		String quack = mallard.performQuack();
+
 		System.out.println(quack);
 	}
 
